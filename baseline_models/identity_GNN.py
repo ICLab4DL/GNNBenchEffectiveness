888e1175fd5999bf4c78f90113d549aba79f6340
lizhengdao <zhengdaoli@link.cuhk.edu.cn>
2022-08-21 13:54:14 +0000

    update idgnn

--- baseline_models/identity_GNN.py
+++ baseline_models/identity_GNN.py
@@ -4,11 +4,12 @@ import numpy as np
 import random
 
 from typing import Callable, Union
+from typing import List, Optional, Set
 from torch_geometric.typing import OptPairTensor, Adj, OptTensor, Size
 import torch
 import torch.nn as nn
 # from torch.functional import F
-# from torch_geometric.nn import GATConv, GINConv, ChebConv, SAGEConv, HypergraphConv
+from torch_geometric.nn import GATConv, GINConv, ChebConv, SAGEConv, HypergraphConv
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric import utils as pygutils
 from torch import Tensor
@@ -34,44 +35,44 @@ class IDGINConv(MessagePassing):
 
 
     def forward(self, x: Union[Tensor, OptPairTensor], edge_index: Adj, edge_index_opt: Adj=None,
-                size: Size = None) -> Tensor:
+                size: Size = None, idx=None, k_nodes=None) -> Tensor:
+        
         if isinstance(x, Tensor):
             x: OptPairTensor = (x, x)
-            
-        out = self.propagate(edge_index, x=x, size=size)
-            
+        out, h0 = self.propagate(edge_index, x=x[0], size=size, idx=idx, k_nodes=k_nodes)
+        
         if edge_index_opt is not None:
             out_opt = self.propagate(edge_index_opt, x=x, size=size)
             out = torch.cat([out, out_opt], dim=-1)
                 
         if edge_index_opt is None:
-            out += (1 + self.eps) * x[0]
+            out += (1 + self.eps) * h0
         else:
-            out += (1 + self.eps) * torch.cat([x[0], x[1]], dim=-1)
+            out += (1 + self.eps) * torch.cat([h0, h0], dim=-1)
 
         return out
 
-    def message_and_aggregate(self, adj_t: SparseTensor,
-                              x: OptPairTensor, idx: int) -> Tensor:
-                              
-        adj_t = adj_t.set_value(None, layout=None)
-        N = len(adj_t[0])
+    # def message(self, x_j: Tensor, idx=None) -> Tensor:
+    #     print('calling mess', idx)
+    #     return x_j
 
-        # NOTE: repeat K times:
+    
+    def message_and_aggregate(self, edge_index: SparseTensor, x=None, idx=None, k_nodes=None ) -> Tensor:
+        N = x.shape[0]
         h0 = self.pre_nn0(x)
-        h1 = self.pre_nn1(x)
-        hk = torch.cat([h0, h1], dim=0)
-
-        hj = x
-        k_nodes, _, _, _ = pygutils.k_hop_subgraph(idx, self.K, adj_t)
+        h1 = self.pre_nn1(x[idx, ...].unsqueeze(0))
+        h_pre = torch.cat([h0, h1], dim=0)
         for j in k_nodes:
-            neighbors = adj_t.index_select(dim=0, idx=torch.tensor([j]))
+            # neighbors = torch.index_select(edge_index, dim=0, index=j) # TODO: pre_calculate
+            neighbors = edge_index.index_select(dim=0, idx=j.unsqueeze(-1))
             neighbors = neighbors.to_torch_sparse_coo_tensor()._indices()[1]
             # NOTE: if n == i then xx[n + N] else: xx[n]
-            neighbors[neighbors==idx] += N
-            hj[j] = torch.sum(hk[neighbors, :], dim=0) # NOTE: aggregate or other agg op.
-
-        return hj
+            neighbors = neighbors.squeeze()
+            neighbors[neighbors==idx] = N
+            
+            h_pre[j] = torch.sum(h_pre[neighbors, :], dim=0) # NOTE: aggregate or other agg op.
+            
+        return h_pre[:N, ...], h0
 
 
 class IDGNN(nn.Module):
@@ -79,31 +80,41 @@ class IDGNN(nn.Module):
         super(IDGNN, self).__init__()
         
         self.args = args
-        
+        self.out_dim = out_dim
+        self.K = 2
         self.convs = nn.ModuleList()
         
-        ln_0 = models.MLP(2*in_dim, hid_dim, hid_dim, 2)
-        ln_1 = models.MLP(2*in_dim, hid_dim, hid_dim, 2)
+        ln_0 = models.MLP(in_dim, hid_dim, hid_dim, 2)
+        ln_1 = models.MLP(in_dim, hid_dim, hid_dim, 2)
         self.convs.append(IDGINConv(ln_0, ln_1))
         
         for _ in range(layer_num - 2):
-            ln_mid0 = models.MLP(2 * hid_dim, hid_dim, hid_dim, 2)
-            ln_mid1 = models.MLP(2 * hid_dim, hid_dim, hid_dim, 2)
+            ln_mid0 = models.MLP(hid_dim, hid_dim, hid_dim, 2)
+            ln_mid1 = models.MLP(hid_dim, hid_dim, hid_dim, 2)
             self.convs.append(IDGINConv(ln_mid0, ln_mid1))
             
-        ln_last0 = models.MLP(2 * hid_dim, hid_dim, out_dim, 2)
-        ln_last1 = models.MLP(2 * hid_dim, hid_dim, out_dim, 2)
+        ln_last0 = models.MLP(hid_dim, hid_dim, out_dim, 2)
+        ln_last1 = models.MLP(hid_dim, hid_dim, out_dim, 2)
 
         self.convs.append(IDGINConv(ln_last0, ln_last1))
         
     def forward(self, x, adj1, adj2=None, graphs:models.BaseGraph=None):
-        N = len(adj1[0])
-        out = torch.empty_like(x[0])
+        
+        sp_index = SparseTensor.from_edge_index(adj1)
+        sp_index.set_value(None)
+        
+        k_adj = sp_index
+        for _ in range(self.K-1):
+            k_adj = matmul(k_adj, sp_index)
+        dense_k_adj = k_adj.to_dense()
+        print(type(dense_k_adj[0]))
+        out = []
+        N = x.shape[0]
         for i in range(N):
             hj = x
             for conv in self.convs:
-                hj = conv(hj, adj1, adj2, idx=i)
-            out[i] = hj[i]
-            
+                hj = conv(hj, sp_index, adj2, idx=i, k_nodes=dense_k_adj[i])
+            out.append(hj[i])
+        out = torch.stack(out, dim=0)
         return out
     
--- training.py
+++ training.py
@@ -2,8 +2,8 @@ import utils
 import models
 from models import *
 
-import baseline_models.gnn_lspe.nets.OGBMOL_graph_classification.gatedgcn_net as lspe_net
-import baseline_models.gnn_lspe.layers.gatedgcn_layer as lspe_layers
+# import baseline_models.gnn_lspe.nets.OGBMOL_graph_classification.gatedgcn_net as lspe_net
+# import baseline_models.gnn_lspe.layers.gatedgcn_layer as lspe_layers
 
 import torch_geometric
 from torch_geometric.nn import global_mean_pool, global_add_pool, global_max_pool, global_sort_pool
@@ -103,8 +103,9 @@ class GraphEvaluator:
     def statistic(self, epoch):
         raise NotImplementedError
 
+
 class SimpleEvaluator(GraphEvaluator):
-    def __init__(self, args):
+    def __init__(self, args, is_regression=False):
         super(SimpleEvaluator, self).__init__()
         self.args = args
         self.metrics = defaultdict(list)
@@ -113,22 +114,46 @@ class SimpleEvaluator(GraphEvaluator):
         self.epoch_loss = []
         self.mean_metrics = defaultdict(list)
         self.total_metrics = {}
+        self.is_regression = is_regression
         
-    def evaluate(self, preds, labels, loss):
-        num = preds.size(0)
-        # print('evl preds shape:', preds.shape, labels.shape)
-        preds_b = preds.argmax(dim=1).squeeze()
-        labels = labels.squeeze()
-        ones = torch.zeros(num)
-        ones[preds_b == labels] = 1
-        acc = torch.sum(ones) / num
+    def evaluate(self, preds, labels, loss, null_val=0.0):
+        
+        if self.is_regression:
+            preds_b = preds
+            if np.isnan(null_val):
+                mask = ~torch.isnan(labels)
+            else:
+                mask = (labels != null_val)
+                mask = mask.float()
+                mask /= torch.mean(mask)
+                # handle all zeros.
+                mask = torch.where(torch.isnan(mask), torch.zeros_like(mask), mask)
+                mse = (preds - labels) ** 2
+                mae = loss
+                mape = mae / labels
+                mae, mape, mse = [mask_and_fillna(l, mask) for l in [mae, mape, mse]]
+                rmse = torch.sqrt(mse)
+                
+                self.metrics['mae'].append(mae.item())
+                self.metrics['mape'].append(mape.item())
+                self.metrics['rmse'].append(rmse.item())
+                self.metrics['loss'].append(loss)
             
-        mi_f1, ma_f1, weighted_f1 = utils.cal_f1(preds_b.cpu().detach().numpy(), labels.cpu().detach().numpy())
-        self.metrics['micro_f1'].append(mi_f1)
-        self.metrics['macro_f1'].append(ma_f1)
-        self.metrics['weighted_f1'].append(weighted_f1)
-        self.metrics['acc'].append(acc.numpy())
-        self.metrics['loss'].append(loss)
+        else:
+            num = preds.size(0)
+            # print('evl preds shape:', preds.shape, labels.shape)
+            preds_b = preds.argmax(dim=1).squeeze()
+            labels = labels.squeeze()
+            ones = torch.zeros(num)
+            ones[preds_b == labels] = 1
+            acc = torch.sum(ones) / num
+                
+            mi_f1, ma_f1, weighted_f1 = utils.cal_f1(preds_b.cpu().detach().numpy(), labels.cpu().detach().numpy())
+            self.metrics['micro_f1'].append(mi_f1)
+            self.metrics['macro_f1'].append(ma_f1)
+            self.metrics['weighted_f1'].append(weighted_f1)
+            self.metrics['acc'].append(acc.numpy())
+            self.metrics['loss'].append(loss)
         
         self.preds.append(preds_b)
         self.labels.append(labels)
@@ -141,12 +166,31 @@ class SimpleEvaluator(GraphEvaluator):
         self.metrics = defaultdict(list)
             
     def eval_on_test(self):
-        preds = torch.cat(self.preds, dim=0)
-        labels = torch.cat(self.labels, dim=0)
-        mi_f1, ma_f1, weighted_f1 = utils.cal_f1(preds.cpu().detach().numpy(), labels.cpu().detach().numpy())
-        self.total_metrics['micro_f1'] = mi_f1
-        self.total_metrics['macro_f1'] = ma_f1
-        self.total_metrics['weighted_f1'] = weighted_f1
+        if self.is_regression:
+            preds = torch.cat(self.preds, dim=0)
+            labels = torch.cat(self.labels, dim=0)
+            mask = (labels != 0.0)
+            mask = mask.float()
+            mask /= torch.mean(mask)
+            # handle all zeros.
+            mask = torch.where(torch.isnan(mask), torch.zeros_like(mask), mask)
+            mse = (preds - labels) ** 2
+            mae = torch.abs(preds - labels)
+            mape = mae / labels
+            mae, mape, mse = [mask_and_fillna(l, mask) for l in [mae, mape, mse]]
+            rmse = torch.sqrt(mse)
+            
+            self.metrics['mae'].append(mae)
+            self.metrics['mape'].append(mape)
+            self.metrics['rmse'].append(rmse)
+            
+        else:
+            preds = torch.cat(self.preds, dim=0)
+            labels = torch.cat(self.labels, dim=0)
+            mi_f1, ma_f1, weighted_f1 = utils.cal_f1(preds.cpu().detach().numpy(), labels.cpu().detach().numpy())
+            self.total_metrics['micro_f1'] = mi_f1
+            self.total_metrics['macro_f1'] = ma_f1
+            self.total_metrics['weighted_f1'] = weighted_f1
     
     def print_info(self):
         micro_f1 = self.metrics[-1]['micro_f1']
@@ -160,19 +204,18 @@ class SimpleEvaluator(GraphEvaluator):
         
     
         
-    def plot_metrics(self, fig_save_path='./figs/'):
-    
+    def plot_metrics(self):
+        
         preds = torch.cat(self.preds, dim=0)
         labels = torch.cat(self.labels, dim=0)
         
-        plot_confuse_matrix(preds, labels, utils.append_tag(fig_save_path, 'confusion.png'))
+        if not self.is_regression:
+            plot_confuse_matrix(preds, labels)
         
         loss_list = self.mean_metrics['loss']
-        correct = int((preds == labels).sum())  # Check against ground-truth labels.
-        acc = correct / labels.shape[0]  # Derive ratio of correct predictions.
-        print('Acc: ', acc)
         
-        plot_loss(loss_list, save_file_path=utils.append_tag(fig_save_path, 'loss.png'))
+        plot_loss(loss_list)
+        
         
 
 def mirror_adj(a: torch.Tensor):
@@ -224,68 +267,103 @@ class DirectModelAdapter(nn.Module):
         out = self.ln(out)
         return out
     
-    
+        
 class GNNModelAdapter(nn.Module):
-    def __init__(self, model, pooling, out_dim, node_fea=None):
+    def __init__(self, model, pooling, out_dim, node_fea=None, mid_fea=False):
         super(GNNModelAdapter, self).__init__()
         self.model = model
+        
+        self.mid_fea = mid_fea
         self.pooling = pooling
         self.node_fea = node_fea
         self.ln = nn.Linear(model.out_dim, out_dim)
         
-    def forward(self, graphs:models.BaseGraph):
+    def forward(self, graphs:BaseGraph):
         node_x, adj = graphs.get_node_features(), graphs.A
         if self.node_fea is not None:
             node_x = self.node_fea
             
-        if graphs.graph_type == 'pyg':
-            edge_index = graphs.pyg_graph.edge_index
+        if graphs.graph_type == 'pyg' or graphs.graph_type == 'coo':
+            edge_indices = graphs.get_edge_index()
+            mid_feature = self.model(node_x, edge_indices, graphs=graphs)
         else:
-            edge_index = adj
-            
-        out = self.model(node_x, edge_index, graphs)
-        out = self.pooling(out, graphs.pyg_graph.batch)
+            mid_feature = self.model(node_x, adj, graphs)
+        
+        print('mid device:', mid_feature.device)
+        print('pyg_graph device:', graphs.pyg_graph.batch.device)
+        out = self.pooling(mid_feature, graphs.pyg_graph.batch) if self.pooling is not None else mid_feature
         out = self.ln(out)
-        return out
+        if self.mid_fea:
+            return out, mid_feature
+        else:
+            return out
+
     
     
 
-def choose_model(args, name, node_fea_dim=1, pooling='gate', graph_fea_dim=1, class_num=6, N=-1):
+def choose_model(args, name, node_fea_dim=1, graph_fea_dim=1, class_num=6, node_num=20,
+                 out_mid_fea=False, node_wise=False):
+    import importlib
     
+    # import baseline_models.gnn_lspe.nets.OGBMOL_graph_classification.gatedgcn_net as lspe_net
+    # import baseline_models.gnn_lspe.layers.gatedgcn_layer as lspe_layers
+
+    import baseline_models.gnn_baselines
+    from baseline_models import identity_GNN
+
+
+    from torch_geometric.nn import global_mean_pool, global_add_pool, global_max_pool, global_sort_pool
+    
+    importlib.reload(models)
+    importlib.reload(identity_GNN)
+    
+    
+    N = node_num
     if name =='mlp':
-        my_model = models.ClassPredictor(64, class_num, 3, dropout=0.6)
-    elif name == 'gcn':
-        # pool = global_mean_pool
-        my_model = models.GCNO(node_fea_dim, 64, class_num)
-        # my_model = GNNModelAdapter(models.GCNO(node_fea_dim, 64), pool, class_num)
-        
+        my_model = models.ClassPredictor(N*N, 64, class_num, 3, dropout=0.6)
     elif name == 'cnn':
         my_model = models.SimpleCNN(graph_fea_dim, 64, class_num, dropout=0.6)
     elif name == 'cnn_big':
         my_model = models.SimpleCNN(graph_fea_dim, 64, class_num, dropout=0.6, kernelsize=(11, 11))
     elif name == 'gnn':
-        if pooling == 'gate':
-            pool =  models.GateGraphPooling(None, N = N)
-        elif pooling == 'mean':
-            pool = global_mean_pool
-        layer_num = 3
+        # pool =  models.GateGraphPooling(None, N = N)
+        pool = global_mean_pool
+        layer_num = 2
         my_model = GNNModelAdapter(models.MultilayerGNN(layer_num, node_fea_dim, 64, 32, dropout=0.6), pool, class_num)
     elif name == 'gin':
-        pool =  models.GateGraphPooling(None, N = N)
+        # pool =  models.GateGraphPooling(None, N = N)
+        if node_wise:
+            pool = None
+        else:
+            pool = models.MeanPooling()
+            # pool = global_mean_pool
         layer_num = 3
-        my_model = GNNModelAdapter(models.GINNet(args, node_fea_dim, 64, 32, 3, dropout=0.6), pool, class_num)
+        my_model = GNNModelAdapter(models.GINNet(args, node_fea_dim, 64, 32, 3, dropout=0.6), pool, class_num, mid_fea=out_mid_fea)
+    elif name == 'sparse_gin':
+        if node_wise:
+            pool = None
+        else:
+            pool = global_mean_pool
+            # pool = global_mean_pool
+        layer_num = 3
+        my_model = GNNModelAdapter(models.LSDGINNet(args, node_fea_dim, 64, 32, 3, dropout=0.6, bi=False), pool, class_num, mid_fea=out_mid_fea)
     elif name == 'gin_direc':
         pool =  models.GateGraphPooling(None, N = N)
         layer_num = 3
         di_model = models.DiGINNet(args, node_fea_dim, 64, 32, 3, dropout=0.6)
         my_model = DirectModelAdapter(di_model, pool, class_num)
     elif name == 'lsd_gin':
+        pool = global_mean_pool
+        pool = global_add_pool
         layer_num = 3
         di_model = models.LSDGINNet(args, node_fea_dim, 64, 32, 3, dropout=0.6)
-        # pool =  models.LinearGraphPooling(32, 32)
-        pool = global_mean_pool
         my_model = DirectModelAdapter(di_model, pool, class_num)
-        
+    
+    elif name == 'idgnn':
+        pool = global_add_pool
+        layer_num = 3
+        idgnn_model = identity_GNN.IDGNN(args, node_fea_dim, 64, 32, layer_num)
+        my_model = GNNModelAdapter(idgnn_model, pool, class_num, mid_fea=out_mid_fea)
     elif name == 'lspe':
         pe_init = 'lap_pe'
         pos_enc_dim = 8
@@ -298,64 +376,96 @@ def choose_model(args, name, node_fea_dim=1, pooling='gate', graph_fea_dim=1, cl
         
         pool =  models.GateGraphPooling(None, N = N)
         my_model = GNNModelAdapter(lspe_model, pool, class_num)
-        
+    else:
+        raise NotImplementedError
     
+        
     my_model.cuda()
     return my_model
 
-def train_svm(train_loader, test_loader):
-    train_x = train_loader.dataset.x
+
+def mask_and_fillna(loss, mask):
+    loss = loss * mask
+    loss = torch.where(torch.isnan(loss), torch.zeros_like(loss), loss)
+    return torch.mean(loss)
+
+
+class MAECal(nn.Module):
+    def __init__(self):
+        super(MAECal, self).__init__()
+        pass
     
-    datasets = dict()
-    print('len:',len(xs))
-    for i in range(len(xs)):
-        datasets[f'{cates[i]}_x'] = xs[i].reshape(xs[i].shape[0],-1)
-        datasets[f'{cates[i]}_y'] = ys[i]
-        print(f'{cates[i]}_y: shape',datasets[f'{cates[i]}_y'].shape)
-        print(f'{cates[i]}_x: shape',datasets[f'{cates[i]}_x'].shape)
+    def forward(self, pred_y, labels, null_val=0.0):
+        
+        labels = labels.squeeze()
+        if np.isnan(null_val):
+            mask = ~torch.isnan(labels)
+        else:
+            mask = (labels != null_val)
+        
+        mask = mask.float()
+        mask /= torch.mean(mask)
+        mask = torch.where(torch.isnan(mask), torch.zeros_like(mask), mask)
 
-    c = Counter(datasets['train_y'])
+        mae = torch.abs(pred_y - labels)
+        mae = mask_and_fillna(mae, mask)
+        return mae
 
-    if os.path.exists('svm_clf.pkl'):
-        clf = joblib.load('svm_clf.pkl')
-        print('loaded svm_clf!!!!!!!')
-    else:
-        clf = svm.SVC()
-        clf.fit(datasets['train_x'], datasets['train_y'])
-        joblib.dump(clf, 'svm_clf.pkl')
-    # mi_f1 = f1_score(datasets['test_y'], preds, average='micro')
-    # ma_f1 = f1_score(datasets['test_y'], preds, average='macro')
-    # weighted_f1 = f1_score(datasets['test_y'], preds, average='weighted')
-    preds = clf.decision_function(test_x)
-    return preds
+class CELossCal(nn.Module):
+    def __init__(self, weights=None):
+        super(CELossCal, self).__init__()
+        self.crite = nn.CrossEntropyLoss(weight=weights)
+        
+    def forward(self, pred_y, y):
+        return self.crite(pred_y, y)
+ 
+ 
+ 
 
-
-def train_gnn(args, train_loader, test_loader, gnn_name='gnn', pooling='gate', epoch=10, plot=False, node_fea_dim=1):
+def train_gnn(args, train_loader, test_loader, gnn_name='gnn', epoch=200, plot=False, node_fea_dim=1,
+              class_num=6, node_num=20, **xargs):
+        
+    def get_value(key, default=None):
+        return xargs[key] if key in xargs else default
+    
     # use GNN to trian:
-    gnn_model = choose_model(args, gnn_name, node_fea_dim=node_fea_dim, pooling=pooling, class_num=args.class_num)
-    if args.cuda:
-        gnn_model.cuda()
-    # opt = optim.Adam(mlp_model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-    # opt = optim.SGD(gnn_model.parameters(), lr=0.001)
-    opt = torch.optim.Adam(gnn_model.parameters(), lr=0.005)
-    ce_loss_cal = CELossCal()
-
-    trainer = utils.Trainer(gnn_model, optimizer=opt, loss_cal=ce_loss_cal)
+    args.debug = False
+    utils.DLog.init(args)
     
-    train_sim_evl= SimpleEvaluator(args)
-    test_sim_evl= SimpleEvaluator(args)
+    is_node_wise = get_value('is_node_wise', False)
+    is_regression = get_value('is_regression', False)
+    scaler = get_value('scaler')
+    opt = get_value('opt', 'sgd')
+    lr = xargs['lr'] if 'lr' in xargs else 0.0002
+    
+    
+    
+    gnn_model = choose_model(args, gnn_name, node_fea_dim=node_fea_dim, class_num=class_num,
+                             node_num=node_num,
+                             node_wise=is_node_wise)
+    if opt == 'adam':
+        opt = optim.Adam(gnn_model.parameters(), lr=lr, weight_decay=args.weight_decay)
+    else:
+        opt = optim.SGD(gnn_model.parameters(), lr=lr)
+        
+    
+    loss_cal = get_value('loss_cal', None)
+    if loss_cal is None:
+        loss_cal =  MAECal() if is_regression else CELossCal()
+    elif loss_cal == 'mse':
+        loss_cal = nn.MSELoss()
+    elif loss_cal == 'mae':
+        loss_cal = MAECal()
+        
+
+    trainer = utils.Trainer(gnn_model, optimizer=opt, loss_cal=loss_cal, scaler=scaler)
+    train_sim_evl= SimpleEvaluator(args, is_regression=is_regression)
+    test_sim_evl= SimpleEvaluator(args,is_regression=is_regression)
 
     training(epoch, trainer, train_sim_evl, test_sim_evl, train_loader, test_loader)
 
     if plot:
-        basedir, file_tag = os.path.split(args.fig_filename)
-        date_dir = time.strftime('%Y%m%d', time.localtime(time.time()))
-        fig_save_dir = os.path.join(basedir, date_dir)
-        if not os.path.exists(fig_save_dir):
-            os.mkdirs(fig_save_dir)
-        fig_save_path = os.path.join(fig_save_dir, f'_{file_tag}_')
+        train_sim_evl.plot_metrics()
+        test_sim_evl.plot_metrics()
         
-        train_sim_evl.plot_metrics(fig_save_path = fig_save_path)
-        test_sim_evl.plot_metrics(fig_save_path = fig_save_path)
-        
-    return train_sim_evl, test_sim_evl
+    return train_sim_evl, test_sim_evl, gnn_model
